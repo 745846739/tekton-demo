4bca8e6bb4397eb05be6607d7583be9f7fddeae4
泽运 <lizezil@163.com>
2024-04-04 12:28:30 +0800

    temp

--- src/main/java/com/lzy/tekton/controller/HelloController.java
+++ src/main/java/com/lzy/tekton/controller/HelloController.java
@@ -2,11 +2,15 @@ package com.lzy.tekton.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("hello")
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/hello1")
+    @ResponseBody
     public String hello(){
-        return "hello";
+        return "hello1";
     }
 }
